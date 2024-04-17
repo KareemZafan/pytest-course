5267d1284f66552bf6eafb37f43bc16dddf396c4
KareemZafan <kareemm.zafan@gmail.com>
2024-04-17 21:25:25 +0200

    adding new task

---
+++ tests/test_strings_copy.py
@@ -0,0 +1,19 @@
+from src import strings as st
+import pytest
+
+@pytest.mark.order(1)
+def test_capitalize():
+    assert st.capitalize("kareem") == "Kareem"
+
+@pytest.mark.order(3)
+def test_end_With():
+    assert st.end_with("kareem","eem") 
+    assert st.end_with("Hello my team, nice to meet you today.","today.") 
+
+
+a = 20 
+payment= 100
+#@pytest.mark.skipif(a<=15, reason="It should work only after day 15")
+@pytest.mark.order(2)
+def test_payment():
+    assert payment != 100
